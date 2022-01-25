13f269be4d315b025022ef435da1cab50f5a060d
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-01-25 07:14:21 +0530

    Create file

--- Easy/panagram_sentence.py
+++ Easy/panagram_sentence.py
@@ -1,10 +1,8 @@
 /* https://leetcode.com/problems/check-if-the-sentence-is-pangram/ */
   
 class Solution:
-    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        allowed = set(allowed)
-        ans = 0
-        for word in words:
-            if set(word).issubset(allowed):
-                ans += 1
-        return ans
+    def checkIfPangram(self, sentence: str) -> bool:
+        if set(sentence) == set('abcdefghijklmnopqrstuvwxyz'):
+            return True
+        else:
+            return False
