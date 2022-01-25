390cc7b3b57e46591ee37ef8b2a9a5fce7ab16dd
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-01-25 07:08:21 +0530

    Create file

---
+++ Easy/truncate_sentence.py
@@ -0,0 +1,10 @@
+/* https://leetcode.com/problems/truncate-sentence/ */
+
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        for i in range(len(s)):
+            if s[i] == ' ':
+                k -= 1
+                if k == 0:
+                    return s[:i]
+        return s
