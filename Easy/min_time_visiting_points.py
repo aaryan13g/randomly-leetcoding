e4fd42c0c2dc97f15977287456872a036634af3f
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-01-27 23:54:16 +0530

    Create min_time_visiting_points.py

---
+++ Easy/min_time_visiting_points.py
@@ -0,0 +1,8 @@
+/* https://leetcode.com/problems/minimum-time-visiting-all-points/ */
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        time = 0
+        for i in range(len(points) - 1):
+            time += max(abs(points[i][0] - points[i + 1][0]), abs(points[i][1] - points[i + 1][1]))
+        return time
