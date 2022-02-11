108983d6952e25c396610003386c072b9e56d580
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-02-12 05:06:07 +0530

    Create palindrome_num.py

---
+++ Easy/palindrome_num.py
@@ -0,0 +1,16 @@
+/* https://leetcode.com/problems/palindrome-number/ */
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        rev = 0
+        orig = x
+        while x > 0:
+            rev = (rev * 10) + (x % 10)
+            x = x // 10
+        print(rev)
+        if rev == orig:
+            return True
+        else:
+            return False
