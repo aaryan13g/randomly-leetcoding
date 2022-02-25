fc5b8977c6d0d2b6419ce51b217c262df038223e
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-02-26 00:03:39 +0530

    Create strstr.py

---
+++ Easy/strstr.py
@@ -0,0 +1,10 @@
+/* https://leetcode.com/problems/implement-strstr/ */
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
