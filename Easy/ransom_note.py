800a78cf9fd98214388ad1935002970d8e243720
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-08-25 10:06:39 +0530

    Create ransom_note.py

---
+++ Easy/ransom_note.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/ransom-note/
+
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter1 = Counter(ransomNote)
+        counter2 = Counter(magazine)
+        if counter1 & counter2 == counter1:
+            return True
+        else:
+            return False
