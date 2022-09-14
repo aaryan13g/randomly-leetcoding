a9d924b72def3f84f7db0191aed3a20bcb5c50a3
Aaryan Gupta <gupta.aaryan8@gmail.com>
2022-09-14 09:23:03 +0530

    Create single_number.py

---
+++ Easy/single_number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for n in nums:
+            xor ^= n
+        return xor
